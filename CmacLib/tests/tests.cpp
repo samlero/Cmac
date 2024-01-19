e57556253806559396fb898ad7bc4e4e1846302c
samlero <scdoctol@gmail.com>
2024-01-19 15:19:49 -0700

    playing around with testing

---
+++ CmacLib/tests/tests.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+//#include "../include/CmacLib.h"
+//#include <CmacLib.h>
+#include <memory>
+
+//using namespace CmacLib;
+using namespace std;
+
+TEST(FACTORY, CreateCmac)
+{
+	// create factory
+	//std::unique_ptr<Factory> factory(new Factory());
+
+	//std::vector<double> upper(2, 10.0);
+	//std::vector<double> lower(2, -10.0);
+	//std::unique_ptr<ICmac> cmac(factory->CreateCmac(10
+	//	, 100, 1e5, 2, upper, lower, 0.001, 0.00001));
+
+	cout << "Hello World" << endl;
+}
