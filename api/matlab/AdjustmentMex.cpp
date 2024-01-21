b2516e2f97915c41ef690da6df23430b829d9d8b
samlero <scdoctol@gmail.com>
2024-01-20 17:25:24 -0700

    initial matlab mex files

---
+++ api/matlab/AdjustmentMex.cpp
@@ -0,0 +1,9 @@
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+class AdjustmentMex : public matlab::mex::Function {
+public:
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs){
+        
+    }
+}
---
+++ api/matlab/FactoryMex.cpp
@@ -0,0 +1,9 @@
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+class FactoryMex : public matlab::mex::Function {
+public:
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs){
+        
+    }
+}
