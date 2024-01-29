e1933dbb0ffe4c8de3a00ae872beb290f72ddd35
aldhinn <aldespinas@gmail.com>
2024-01-29 14:11:05 -0700

    explicitly stated return value policies.

--- api/python/src/lib.cpp
+++ api/python/src/lib.cpp
@@ -21,49 +21,49 @@ using namespace CmacLib;
 PYBIND11_MODULE(cmaclib, m) {
     pybind11::class_<IResult,
         ::std::unique_ptr<IResult, pybind11::nodelete>>(m, "IResult")
-        .def("IsSuccessful", &IResult::IsSuccessful)
-        .def("GetMessage", &IResult::GetMessage);
+        .def("IsSuccessful", &IResult::IsSuccessful, pybind11::return_value_policy::take_ownership)
+        .def("GetMessage", &IResult::GetMessage, pybind11::return_value_policy::take_ownership);
     pybind11::class_<Result, IResult>(m, "Result");
 
     pybind11::class_<ISerialization, IResult,
         ::std::unique_ptr<ISerialization, pybind11::nodelete>>(m, "ISerialization")
-        .def("GetString", &ISerialization::GetString);
+        .def("GetString", &ISerialization::GetString, pybind11::return_value_policy::take_ownership);
     pybind11::class_<Serialization, ISerialization, Result>(m, "Serialization");
 
     pybind11::class_<IPrediction, IResult,
         ::std::unique_ptr<IPrediction, pybind11::nodelete>>(m, "IPrediction")
-        .def("GetValues", &IPrediction::GetValues)
-        .def("GetActiveWeightIndices", &IPrediction::GetActiveWeightIndices)
-        .def("GetActiveWeights", &IPrediction::GetActiveWeights)
-        .def("GetBasisValues", &IPrediction::GetBasisValues);
+        .def("GetValues", &IPrediction::GetValues, pybind11::return_value_policy::reference_internal)
+        .def("GetActiveWeightIndices", &IPrediction::GetActiveWeightIndices, pybind11::return_value_policy::reference_internal)
+        .def("GetActiveWeights", &IPrediction::GetActiveWeights, pybind11::return_value_policy::reference_internal)
+        .def("GetBasisValues", &IPrediction::GetBasisValues, pybind11::return_value_policy::reference_internal);
     pybind11::class_<Prediction, IPrediction, Result>(m, "Prediction");
 
     pybind11::class_<IAdjustment, IResult,
         ::std::unique_ptr<IAdjustment, pybind11::nodelete>>(m, "IAdjustment")
-        .def("GetWeightChanges", &IAdjustment::GetWeightChanges);
+        .def("GetWeightChanges", &IAdjustment::GetWeightChanges, pybind11::return_value_policy::reference_internal);
     pybind11::class_<Adjustment, IAdjustment, Result>(m, "Adjustment");
 
     pybind11::class_<IMarshaller,
         ::std::unique_ptr<IMarshaller, pybind11::nodelete>>(m, "IMarshaller")
-        .def("Save", &IMarshaller::Save)
-        .def("Load", &IMarshaller::Load);
+        .def("Save", &IMarshaller::Save, pybind11::return_value_policy::take_ownership)
+        .def("Load", &IMarshaller::Load, pybind11::return_value_policy::take_ownership);
     pybind11::class_<Marshaller, IMarshaller>(m, "Marshaller");
 
     pybind11::class_<ISerializable,
         ::std::unique_ptr<ISerializable, pybind11::nodelete>>(m, "ISerializable")
-        .def("Serialize", &ISerializable::Serialize)
-        .def("Deserialize", &ISerializable::Deserialize);
+        .def("Serialize", &ISerializable::Serialize, pybind11::return_value_policy::take_ownership)
+        .def("Deserialize", &ISerializable::Deserialize, pybind11::return_value_policy::take_ownership);
 
     pybind11::class_<ICmac, ISerializable,
         ::std::unique_ptr<ICmac, pybind11::nodelete>>(m, "ICmac")
-        .def("Predict", &ICmac::Predict)
-        .def("Adjust", &ICmac::Adjust);
+        .def("Predict", &ICmac::Predict, pybind11::return_value_policy::take_ownership)
+        .def("Adjust", &ICmac::Adjust, pybind11::return_value_policy::take_ownership);
 
     pybind11::class_<Cmac, ICmac>(m, "Cmac");
 
     pybind11::class_<Factory>(m, "Factory")
         .def(pybind11::init<>())
-        .def("CreateCmac", &Factory::CreateCmac)
-        .def("CreateDefaultCmac", &Factory::CreateDefaultCmac)
-        .def("CreateMarshaller", &Factory::CreateMarshaller);
+        .def("CreateCmac", &Factory::CreateCmac, pybind11::return_value_policy::take_ownership)
+        .def("CreateDefaultCmac", &Factory::CreateDefaultCmac, pybind11::return_value_policy::take_ownership)
+        .def("CreateMarshaller", &Factory::CreateMarshaller, pybind11::return_value_policy::take_ownership);
 }
