3543c8677650482333985c26654be6c5c6a01887
aldhinn <aldespinas@gmail.com>
2024-03-01 08:07:14 -0700

    added shared symbol modifiers in collection data structures.

--- core/include/cmaclib/collections.h
+++ core/include/cmaclib/collections.h
@@ -1,12 +1,14 @@
 #if !defined(CMACLIB_COLLECTIONS_HEADER_FILE)
 #define CMACLIB_COLLECTIONS_HEADER_FILE
 
+#include <cmaclib/defines.h>
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
 /// @brief An array of doubles with fixed array size.
-typedef struct CDoubleArray {
+typedef struct CMACLIB_SHARED_SYMBOL CDoubleArray {
     /// @brief The pointer to the first element in the array.
     double* data;
     /// @brief The size of the array.
@@ -14,7 +16,7 @@ typedef struct CDoubleArray {
 } CDoubleArray;
 
 /// @brief An array of CDoubleArray with fixed array size.
-typedef struct CDoubleArrayArray {
+typedef struct CMACLIB_SHARED_SYMBOL CDoubleArrayArray {
     /// @brief The pointer to the first element in the array.
     CDoubleArray* data;
     /// @brief The size of the array.
@@ -23,10 +25,10 @@ typedef struct CDoubleArrayArray {
 
 /// @brief Properly destroy the CDoubleArrayArray instance.
 /// @param arr The instance to be destroyed.
-void CDoubleArrayArrayDestroy(CDoubleArrayArray arr);
+void CMACLIB_SHARED_SYMBOL CDoubleArrayArrayDestroy(CDoubleArrayArray arr);
 
 /// @brief An array of unsigned ints with fixed array size.
-typedef struct CUintArray {
+typedef struct CMACLIB_SHARED_SYMBOL CUintArray {
     /// @brief The pointer to the first element in the array.
     unsigned int* data;
     /// @brief The size of the array.
