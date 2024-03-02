8a4f00fdcc6f52b758581bceabd38c06350b0221
aldhinn <aldespinas@gmail.com>
2024-03-02 05:22:52 -0700

    wrote marshaller.jl.

---
+++ wrappers/julia/src/marshaller.jl
@@ -0,0 +1,60 @@
+#!/usr/bin/julia
+
+export IMarshaller
+"""
+Save and load serializable objects.
+"""
+struct IMarshaller end
+
+export IMarshallerSave
+"""
+Save the serializable object.
+
+## Arguments
+- `pInst::Ptr{IMarshaller}`: The pointer to the marshaller instance.
+- `pSerializable::Ptr{ISerializable}`: Object to save.
+- `directory::Cstring`: Folder to save the serializable object in.
+- `filename::Cstring`: Name of the object to save.
+
+## Returns
+Indicates if the save operation was successful or not.
+"""
+function IMarshallerSave(pInst::Ptr{IMarshaller},
+        pSerializable::Ptr{ISerializable}, directory::Cstring,
+        filename::Cstring)::Ptr{IResult}
+    return ccall((:CCmacLibIMarshallerSave, cmaclib_core),
+        Ptr{IResult}, (Ptr{IMarshaller}, Ptr{ISerializable}, Cstring, Cstring),
+        pInst, pSerializable, directory, filename)
+end
+
+export IMarshallerLoad
+"""
+Fills the contents of the serialzable object.
+
+## Arguments
+- `pInst::Ptr{IMarshaller}`: The pointer to the marshaller instance.
+- `pSerializable::Ptr{ISerializable}`: Object to fill.
+- `filepath::Cstring`: Location of the file that contains the serializable
+object's contents.
+
+## Returns
+Indicates if the save operation was successful or not.
+"""
+function IMarshallerLoad(pInst::Ptr{IMarshaller},
+        pSerializable::Ptr{ISerializable}, filepath::Cstring)::Ptr{IResult}
+    return ccall((:CCmacLibIMarshallerLoad, cmaclib_core),
+        Ptr{IResult}, (Ptr{IMarshaller}, Ptr{ISerializable}, Cstring),
+        pInst, pSerializable, filepath)
+end
+
+export IMarshallerDestroy
+"""
+Destructor.
+
+## Arguments
+- `pInst`: The pointer to the marshaller instance.
+"""
+function IMarshallerDestroy(pInst::Ptr{IMarshaller})::Nothing
+    ccall((:CCmacLibIMarshallerDestroy, cmaclib_core),
+        Cvoid, (Ptr{IMarshaller}, ), pInst)
+end
